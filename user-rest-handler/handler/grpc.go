feb3b858e05a5a99af76b8d640264cf7d696056e
J-hoplin1 <jhoplin7259@gmail.com>
2024-04-15 00:14:23 +0900

    Feat: Initialize grpc handlers

--- user-rest-handler/handler/grpc.go
+++ user-rest-handler/handler/grpc.go
@@ -1,23 +1,25 @@
 package handler
 
 import (
+	"head-api/service"
+
 	"github.com/gin-gonic/gin"
 )
 
 func EnrollGrpcHandler(router *gin.Engine) {
 	group := router.Group("grpc")
 	{
-		group.GET("unary", func(ctx *gin.Context) {})
+		group.GET("unary", service.UnaryHandler)
 		subGroup := group.Group("stream")
 		{
 			// Client Stream
-			subGroup.GET("client", func(ctx *gin.Context) {})
+			subGroup.GET("client", service.ClientStreamHandler)
 
 			// Server Stream
-			subGroup.GET("server", func(ctx *gin.Context) {})
+			subGroup.GET("server", service.ServerStreamHandler)
 
 			// Bi-directional Stream
-			subGroup.GET("bi", func(ctx *gin.Context) {})
+			subGroup.GET("bi", service.BiDirectionalStreamHandler)
 		}
 	}
 }
